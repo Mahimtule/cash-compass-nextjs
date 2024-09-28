24852e08769453c0b990d579e227834535ffd1d4
Mueez Mahimtule <mahimtulemueez@gmail.com>
2024-09-28 13:36:59 +0530

    V1 Update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import FAQ from "@/components/FAQ";
 import Features from "@/components/Features";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
-import NavBar from "@/components/NavBar";
 
 const page = () => {
   return (
